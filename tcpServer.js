3dfef9c4eb9b1b604c1f9f02c1f42008342e9e41
Melvin Gruesbeck <mgruesbeck@gmail.com>
2017-09-29 01:22:19 -0700

    finish tcp server

--- tcpServer.js
+++ tcpServer.js
@@ -1,2 +1,14 @@
 var net = require('net');
-var server = net.create
+var server = net.createServer(function(connection){
+  console.log('client connected');
+  connection.on('data', function(d){
+    console.log('data received: ' + d.toString());
+  });
+  connection.on('end', function(){
+    console.log('client disconnected');
+  });
+});
+
+server.listen(3000, function(){
+  console.log('server started');
+});
